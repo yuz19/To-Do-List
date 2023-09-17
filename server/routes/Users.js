c4f9611d58f7ffe39799efa59e01572d4d2b7829
nabil19 <funity111@gmail.com>
2023-09-17 01:09:51 +0100

    test2

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -2,10 +2,25 @@ const express=require('express')
 const Users=require('../models/Users')
 const router = express.Router()
 const multer=require('multer')
+
+///Email-find
+router.post('/Email-find',multer().none(),(req,res)=>{
+    // console.log(req.body.Email)
+    Users.find({Email:req.body.Email})
+                .then((result)=>{
+                    res.send(result)
+                })
+                .catch((err)=>{
+                    console.log(err)
+                })
+})
+
+ 
 //signUp
 router.post('/add-user',multer().none(),(req,res)=>{
     const user=new Users({
-        UserName:req.body.userName,
+        UserName:req.body.UserName,
+        Email:req.body.Email,
         Password:req.body.Password
     });
     user.save()
@@ -15,5 +30,17 @@ router.post('/add-user',multer().none(),(req,res)=>{
     .catch((err)=>{
         console.log(err)
     })
+})
+//signIn
+router.post('/sign-in',multer().none(),(req,res)=>{
+    EmailEntre=req.body.Email;
+    Users.find({Email:EmailEntre})
+                .then((result)=>{
+
+                    res.send(result)
+                })
+                .catch((err)=>{
+                    console.log(err)
+                })
 })
 module.exports=router
